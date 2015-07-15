2ba71d1711023f5cf30fb2ab5daabefebb9d1065
Caitlin Stein <caitlin.teter@gmail.com>
2015-07-15 09:05:23 -0500

    javascript works! (but it's not pretty)

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
 var songs = [];
 
-songs[songs.length] = "Legs - by ZZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song - by Supertramp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall - by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welcome to the Jungle - by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironic - by Alanis Morisette on the album Jagged Little Pill";
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+
+
+
+for (var i = 0; i < songs.length; i++) {
+	var songName = songs[i];
+	var newSongName = songName.replace(/\*/g, "");
+	// var newSongName = songName.replace(/\!/g, "");
+	// var newSongName = songName.replace(/\@/g, "");
+	// var newSongName = songName.replace(/\>/g, "-");
+	songs[i] = newSongName;
+}
+for (var i = 0; i < songs.length; i++) {
+	var songName = songs[i];
+	// var newSongName = songName.replace(/\*/g, "");
+	var newSongName = songName.replace(/\!/g, "");
+	// var newSongName = songName.replace(/\@/g, "");
+	// var newSongName = songName.replace(/\>/g, "-");
+	songs[i] = newSongName;
+}
+for (var i = 0; i < songs.length; i++) {
+	var songName = songs[i];
+	// var newSongName = songName.replace(/\*/g, "");
+	// var newSongName = songName.replace(/\!/g, "");
+	var newSongName = songName.replace(/\@/g, "");
+	// var newSongName = songName.replace(/\>/g, "-");
+	songs[i] = newSongName;
+}
+for (var i = 0; i < songs.length; i++) {
+	var songName = songs[i];
+	// var newSongName = songName.replace(/\*/g, "");
+	// var newSongName = songName.replace(/\!/g, "");
+	// var newSongName = songName.replace(/\@/g, "");
+	var newSongName = songName.replace(/\>/g, "-");
+	songs[i] = newSongName;
+}
+for (var i = 0; i < songs.length; i++) {
+	var songName = songs[i];
+	// var newSongName = songName.replace(/\*/g, "");
+	var newSongName = songName.replace(/\(/g, "");
+	// var newSongName = songName.replace(/\@/g, "");
+	// var newSongName = songName.replace(/\>/g, "-");
+	songs[i] = newSongName;
+}
 
 for (var i = 0; i < songs.length; i++) {
 	document.getElementById("library").innerHTML += "<p class='songItem'>" + songs[i] + "</p>";
 }
+
+var phrase = "The lazy dog likes the weird fox";
+var newPhrase = phrase.replace(/o/g, "i");
